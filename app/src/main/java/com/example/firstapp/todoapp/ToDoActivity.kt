a499d4f8597c484535a2d871992ac065e7d89ed5
Lautaro-io <chelo43360@gmail.com>
2024-11-30 01:14:13 -0300

    .

--- app/src/main/java/com/example/firstapp/todoapp/ToDoActivity.kt
+++ app/src/main/java/com/example/firstapp/todoapp/ToDoActivity.kt
@@ -2,7 +2,11 @@ package com.example.firstapp.todoapp
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,12 +17,12 @@ import com.example.firstapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ToDoActivity : AppCompatActivity() {
-    private val categories = listOf<TaskCategory>(
+    private val categories = listOf<TaskCategory>( // lista de categorias
         TaskCategory.Other,
         TaskCategory.Business,
         TaskCategory.Personal
     )
-    private val tasks = mutableListOf<Task>(
+    private val tasks = mutableListOf<Task>( // lista de tareas que hacer
         Task("Prueba", TaskCategory.Business),
         Task("Prueba2", TaskCategory.Personal),
         Task("Prueba3", TaskCategory.Other)
@@ -56,7 +60,7 @@ class ToDoActivity : AppCompatActivity() {
         rvCategories.adapter = categoriesAdapter
 
         tasksAdapter = TasksAdapter(tasks)
-        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.layoutManager = LinearLayoutManager(this) // como el segundo recycleview es horizontal no hace falta asignarle el linearlayoutmanager.horizontal y false
         rvTasks.adapter = tasksAdapter
     }
 
@@ -66,8 +70,29 @@ class ToDoActivity : AppCompatActivity() {
         }
     }
     private fun showDialog(){
-        val dialog = Dialog(this)
-        dialog.setContentView(R.layout.dialog_tasks)
-        dialog.show()
+        val dialog = Dialog(this)//Creamos la ventana pop up
+        dialog.setContentView(R.layout.dialog_tasks) // le seteamos el layout creado previamente
+        dialog.show() // lo mostramos
+
+        val btnAddTask:Button = dialog.findViewById(R.id.btnAddTask) //DEL DIALOGO, buscamos por id los componentes como el boton y el campo de entrada de texto
+        val editTask: EditText = dialog.findViewById(R.id.editTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories) // rg = radiusGroup , contenedor de checkboxs circulares unicos
+
+        btnAddTask.setOnClickListener{ //evento al boton
+            val selectedId = rgCategories.checkedRadioButtonId //obtenemos el id del checkbox seleccionado
+            val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId) // lo buscamos
+            val currentCategory: TaskCategory = when(selectedRadioButton.text){ //hacemos una constante con el valor que contenga el selectedRadioButton
+                "Negocios" -> TaskCategory.Business // si es "Negocios se le asigna negocios"
+                "Personal" -> TaskCategory.Personal
+                else -> TaskCategory.Other
+            }
+            tasks.add(Task(editTask.text.toString() ,currentCategory)) // a la lista previamente creada o inicializada vacia, se le agrega un objeto Task con los valores puestos en el dialog
+            updateTask() //hay que mandarle la informacion al adaptador para que realice los cambios necesarios, por el contrario no mostraria las nuevas tasks
+            dialog.hide()//escondemos el dialogo
+
+        }
+    }
+    private fun updateTask(){
+        tasksAdapter.notifyDataSetChanged()
     }
 }
